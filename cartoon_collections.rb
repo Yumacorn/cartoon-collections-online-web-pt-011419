5ede271bb6e5cb3cd7d89a7e911738a0d092cfee
Ryan Mui <reachryanmui@gmail.com>
2019-02-02 23:58:07 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,5 +12,5 @@ end
 
 def find_the_cheese(assortment)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  assortment.detect {|item| item.include?{cheese_types}}
+  assortment.each {|item| item.include?{cheese_types}}
 end
